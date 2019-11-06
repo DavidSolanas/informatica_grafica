ff12b39da5e740bfaefd58501964cc7fe12244c1
DavidSolanas <davidsolanas086@gmail.com>
2019-11-06 10:56:10 +0100

    Makefile

---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/david/Documents/informatica_grafica/src/Geometry.cpp" "/Users/david/Documents/informatica_grafica/build/CMakeFiles/test.dir/src/Geometry.cpp.o"
+  "/Users/david/Documents/informatica_grafica/src/Plane.cpp" "/Users/david/Documents/informatica_grafica/build/CMakeFiles/test.dir/src/Plane.cpp.o"
+  "/Users/david/Documents/informatica_grafica/src/Scene.cpp" "/Users/david/Documents/informatica_grafica/build/CMakeFiles/test.dir/src/Scene.cpp.o"
+  "/Users/david/Documents/informatica_grafica/src/Sphere.cpp" "/Users/david/Documents/informatica_grafica/build/CMakeFiles/test.dir/src/Sphere.cpp.o"
+  "/Users/david/Documents/informatica_grafica/src/Tone_mapping.cpp" "/Users/david/Documents/informatica_grafica/build/CMakeFiles/test.dir/src/Tone_mapping.cpp.o"
+  "/Users/david/Documents/informatica_grafica/src/Transformation.cpp" "/Users/david/Documents/informatica_grafica/build/CMakeFiles/test.dir/src/Transformation.cpp.o"
+  "/Users/david/Documents/informatica_grafica/src/test.cpp" "/Users/david/Documents/informatica_grafica/build/CMakeFiles/test.dir/src/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/Geometry.cpp.o"
+  "CMakeFiles/test.dir/src/Plane.cpp.o"
+  "CMakeFiles/test.dir/src/Scene.cpp.o"
+  "CMakeFiles/test.dir/src/Sphere.cpp.o"
+  "CMakeFiles/test.dir/src/Tone_mapping.cpp.o"
+  "CMakeFiles/test.dir/src/Transformation.cpp.o"
+  "CMakeFiles/test.dir/src/test.cpp.o"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
