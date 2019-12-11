30735c12d49a5b38596bc32f5cfbe5616bb2b513
santibuey <38045718+santibuey@users.noreply.github.com>
2019-12-11 16:08:19 +0100

    Paralelizacion

--- src/Ray_tracer.cpp
+++ src/Ray_tracer.cpp
@@ -2,7 +2,7 @@
  * Fichero: Ray_tracer.cpp
  * Autor: David Solanas, Santiago Buey
  *****************************************/
-
+#define _USE_MATH_DEFINES
 #include "Ray_tracer.hpp"
 #include "BRDF.hpp"
 #include "Sphere.hpp"
@@ -12,10 +12,205 @@
 #include "Textures.hpp"
 #include "Transformation.hpp"
 #include "Light.hpp"
+#include "Semaphore.hpp"
 #include <iostream>
 #include <random>
 #include <memory>
 #include <cstring>
+#include <thread>
+#include <ctime> 
+
+//Para la ejecucion en paralelo
+void hiloRayTracer(std::ofstream& _f, const int n_ray, Camera c,
+				int inicioX, int finX, int inicioY, int finY, const int H, const int W, 
+				std::random_device& rd, std::mt19937& mt, std::uniform_real_distribution<float> dist, Semaphore& s0,Semaphore& s1, int id){
+	
+    std::vector<std::vector<RGB>> data = load_texture("C:\\Users\\santi\\Desktop\\earthmap1k.ppm");
+	std::array<std::unique_ptr<Geometry>, 7> geometry = scene3(c, W, H);
+	
+	float power = 3600000;
+    PointLight light(Point(W / 2, H - 200, c.getF().mod() - 500), power, RGB(255, 255, 255));	
+	
+	
+	for (int y = finY; y > inicioY; y=y-1)
+	{	
+		for (int x = inicioX; x < finX; x+=2)
+		{
+			float Lo = 0;
+			int i;
+			Point X;
+			for (int k = 0; k < n_ray; k++)
+			{
+				i = -1;
+				float xrand = dist(mt), yrand = dist(mt);
+				Point pixel(x + xrand, y + yrand, c.getF().mod());
+				Direction d_ray = normalize(pixel - c.getO()); 	//Rayo que va de la camara al pixel
+				float t, tmin = INFINITY;
+				for (int j = 0; j < geometry.size(); j++) //Para todos los elementos de la escena
+				{
+					if (geometry[j]->intersect(c.getO(), d_ray, t))
+					{
+						if (t > 0 && t < tmin)
+						{	
+							tmin = t;
+							i = j;
+						}
+					}
+				}
+				if (i != -1)
+				{
+					// Calcular luz puntual
+					X = c.getO() + (d_ray * tmin);
+					float Li = power / ((light.p - X).mod() * (light.p - X).mod());
+					Direction normal = geometry[i]->getNormal(X);
+					Direction wi = normalize(light.p - X);
+					Direction wo = normalize(c.getO() - X);
+					float kd = i == 5 ? 0.9f : 0.6f;
+					float ks = i == 5 ? 0.05f : 0.25f;
+					float brdf = phong_BRDF(kd, ks, 10, normal, wi, wo);
+					float g = dot(normal, wi) < 0 ? 0 : dot(normal, wi);
+					Lo += Li * brdf * g;
+				}
+			}
+			if (i != -1)
+			{
+				RGB color;
+				Sphere s;
+				Cylinder cy;
+				Triangle t;
+				BoundedPlane bp;
+				Cone cone;
+				Direction normal;
+				float u, v;
+				switch (i)
+				{
+				case 0:
+					color.setR(240);
+					color.setG(240);
+					color.setB(240);
+					break;
+				case 1:
+					color.setR(240);
+					color.setG(240);
+					color.setB(240);
+					break;
+				case 2:
+					bp = *reinterpret_cast<BoundedPlane *>((geometry[i].get()));
+					bp.get_uv(X, u, v);
+					color = get_pixel(data, u, v);
+					break;
+				case 3:
+					color.setR(240);
+					color.setG(240);
+					color.setB(240);
+					break;
+				case 4:
+					color.setR(240);
+					color.setG(240);
+					color.setB(240);
+					break;
+				case 5:
+					normal = geometry[i]->getNormal(X);
+					cy = *reinterpret_cast<Cylinder *>((geometry[i].get()));
+					cy.get_uv(normal, X.getCoord()[1] - cy.get_base_Y_coord(), u, v);
+					color = get_pixel(data, u, v);
+					break;
+				case 6:
+					s = *reinterpret_cast<Sphere *>((geometry[i].get()));
+					normal = normalize(s.getCenter() - X);
+					s.get_uv(normal, u, v);
+					color = get_pixel(data, u, v);
+					break;
+				case 7:
+					normal = geometry[i]->getNormal(X);
+					cone = *reinterpret_cast<Cone *>((geometry[i].get()));
+					cone.get_uv(normal, cone.get_vertex_Y_coord() - X.getCoord()[1], u, v);
+					color = get_pixel(data, u, v);
+					break;
+				case 8:
+				case 9:
+				case 10:
+					color.setR(93);
+					color.setG(173);
+					color.setB(164);
+					break;
+
+				case 11:
+					color.setR(255);
+					color.setG(143);
+					color.setB(0);
+					break;
+				case 14:
+					color.setR(0);
+					color.setG(255);
+					color.setB(0);
+					break;
+
+				default:
+					color.setR(141);
+					color.setG(141);
+					color.setB(141);
+					break;
+				}
+				Lo /= n_ray;
+				
+				if (id==0){
+					s0.wait();
+				}
+				else if (id==1){
+					s1.wait();
+				}
+				
+				_f << Lo * color.getR() << " "
+				   << Lo * color.getG() << " "
+				   << Lo * color.getB() << "\t";
+				
+				if (id==0){
+					s1.signal();
+				}
+				else if (id==1){
+					s0.signal();
+				}	
+				
+			}
+			else
+			{			// Si i=-1:
+				
+				if (id==0){
+					s0.wait();
+				}
+				else if (id==1){
+					s1.wait();
+				}
+				
+				_f << 255 << " " << 255 << " " << 255 << "\t";  	// Imprime 1920 255s en la primera linea: ¿por que?
+				
+				if (id==0){
+					s1.signal();
+				}
+				else if (id==1){
+					s0.signal();
+				}	
+			}
+		}
+		
+		if (id==0){
+			s0.wait();
+		}
+		else if (id==1){
+			s1.wait();
+		}
+		
+		_f << "\n";
+		
+		if (id==0){
+			s1.signal();
+		}
+		else if (id==1){
+			s0.signal();
+		}
+	}
+}
 
 void ray_tracer(std::string filename, const int n_ray, Camera c, const int W, const int H)
 {
@@ -23,7 +218,7 @@ void ray_tracer(std::string filename, const int n_ray, Camera c, const int W, co
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<float> dist(0.0f, 1.0f);
-    std::vector<std::vector<RGB>> data = load_texture("/Users/david/Desktop/earthmap1k.ppm");
+    std::vector<std::vector<RGB>> data = load_texture("C:\\Users\\santi\\Desktop\\earthmap1k.ppm");
 
     std::array<std::unique_ptr<Geometry>, 7> geometry = scene3(c, W, H);
 
@@ -40,145 +235,31 @@ void ray_tracer(std::string filename, const int n_ray, Camera c, const int W, co
         int finX = c.getO().getCoord()[0] + c.getL().mod();
         int inicioY = c.getO().getCoord()[1] - c.getU().mod();
         int finY = c.getO().getCoord()[1] + c.getU().mod();
-        for (int y = finY; y > inicioY; y--)
-        {
-            for (int x = inicioX; x < finX; x++)
-            {
-                float Lo = 0;
-                int i;
-                Point X;
-                for (int k = 0; k < n_ray; k++)
-                {
-                    i = -1;
-                    float xrand = dist(mt), yrand = dist(mt);
-                    Point pixel(x + xrand, y + yrand, c.getF().mod());
-                    Direction d_ray = normalize(pixel - c.getO());
-                    float t, tmin = INFINITY;
-                    for (int j = 0; j < geometry.size(); j++)
-                    {
-                        if (geometry[j]->intersect(c.getO(), d_ray, t))
-                        {
-                            if (t > 0 && t < tmin)
-                            {
-                                tmin = t;
-                                i = j;
-                            }
-                        }
-                    }
-                    if (i != -1)
-                    {
-                        // Calcular luz puntual
-                        X = c.getO() + (d_ray * tmin);
-                        float Li = power / ((light.p - X).mod() * (light.p - X).mod());
-                        Direction normal = geometry[i]->getNormal(X);
-                        Direction wi = normalize(light.p - X);
-                        Direction wo = normalize(c.getO() - X);
-                        float kd = i == 5 ? 0.9f : 0.6f;
-                        float ks = i == 5 ? 0.05f : 0.25f;
-                        float brdf = phong_BRDF(kd, ks, 10, normal, wi, wo);
-                        float g = dot(normal, wi) < 0 ? 0 : dot(normal, wi);
-                        Lo += Li * brdf * g;
-                    }
-                }
-                if (i != -1)
-                {
-                    RGB color;
-                    Sphere s;
-                    Cylinder cy;
-                    Triangle t;
-                    BoundedPlane bp;
-                    Cone cone;
-                    Direction normal;
-                    float u, v;
-                    switch (i)
-                    {
-                    case 0:
-                        color.setR(240);
-                        color.setG(240);
-                        color.setB(240);
-                        break;
-                    case 1:
-                        color.setR(240);
-                        color.setG(240);
-                        color.setB(240);
-                        break;
-                    case 2:
-                        bp = *reinterpret_cast<BoundedPlane *>((geometry[i].get()));
-                        bp.get_uv(X, u, v);
-                        color = get_pixel(data, u, v);
-                        break;
-                    case 3:
-                        color.setR(240);
-                        color.setG(240);
-                        color.setB(240);
-                        break;
-                    case 4:
-                        color.setR(240);
-                        color.setG(240);
-                        color.setB(240);
-                        break;
-                    case 5:
-                        normal = geometry[i]->getNormal(X);
-                        cy = *reinterpret_cast<Cylinder *>((geometry[i].get()));
-                        cy.get_uv(normal, X.getCoord()[1] - cy.get_base_Y_coord(), u, v);
-                        color = get_pixel(data, u, v);
-                        break;
-                    case 6:
-                        s = *reinterpret_cast<Sphere *>((geometry[i].get()));
-                        normal = normalize(s.getCenter() - X);
-                        s.get_uv(normal, u, v);
-                        color = get_pixel(data, u, v);
-                        break;
-                    case 7:
-                        normal = geometry[i]->getNormal(X);
-                        cone = *reinterpret_cast<Cone *>((geometry[i].get()));
-                        cone.get_uv(normal, cone.get_vertex_Y_coord() - X.getCoord()[1], u, v);
-                        color = get_pixel(data, u, v);
-                        break;
-                    case 8:
-                    case 9:
-                    case 10:
-                        color.setR(93);
-                        color.setG(173);
-                        color.setB(164);
-                        break;
+		
+		// EJECUCION EN PARALELO
+		int numProcesos=2;
+		std::thread P[numProcesos];
 
-                    case 11:
-                        color.setR(255);
-                        color.setG(143);
-                        color.setB(0);
-                        break;
-                    case 14:
-                        color.setR(0);
-                        color.setG(255);
-                        color.setB(0);
-                        break;
-
-                    default:
-                        color.setR(141);
-                        color.setG(141);
-                        color.setB(141);
-                        break;
-                    }
-                    Lo /= n_ray;
-                    _f << Lo * color.getR() << " "
-                       << Lo * color.getG() << " "
-                       << Lo * color.getB() << "\t";
-                }
-                else
-                {
-                    _f << 255 << " " << 255 << " " << 255 << "\t";
-                }
-            }
-            _f << "\n";
-        }
+		Semaphore s0(1);
+		Semaphore s1(0);
+//		Semaphore turno[numProcesos];
+//		turno[0]=s0;
+		
+		P[0]= std::thread(&hiloRayTracer, ref(_f), n_ray,c,inicioX,finX-1,inicioY,finY-1,H,W,ref(rd),ref(mt), dist, ref(s0), ref(s1), 0); //Impares
+		P[1]= std::thread(&hiloRayTracer, ref(_f), n_ray,c,inicioX+1,finX,inicioY+1,finY,H,W,ref(rd),ref(mt), dist, ref(s0), ref(s1), 1);
+//		P[2]= std::thread(&hiloRayTracer, ref(_f), n_ray,c,inicioX,(finX/2)-1,inicioY,(finY/2)-1,H,W,ref(rd),ref(mt), dist);
+//		P[3]= std::thread(&hiloRayTracer, ref(_f), n_ray,c,finX/2,finX,inicioY,(finY/2)-1,H,W,ref(rd),ref(mt), dist);
+		for (int i=0;i<numProcesos;i++){
+			P[i].join();
+		}
     }
     _f.close();
 }
 
 int main(int argc, const char **argv)
 {
-
+	unsigned t0= clock();
+	
     std::string usage = "Usage: Ray_tracer -n n_rays";
     if (argc != 3)
     {
@@ -216,7 +297,10 @@ int main(int argc, const char **argv)
         Direction f(0, 0, u.mod() / tan(M_PI / 24));
         Point c0((int)W / 2, (int)H / 2, 0);
         Camera c(f, u, l, c0);
-        ray_tracer("/Users/david/Desktop/prueba.ppm", n_ray, c, W, H);
+        ray_tracer("C:\\Users\\santi\\Desktop\\prueba.ppm", n_ray, c, W, H);
     }
+	unsigned t1= clock();
+	double time = (double(t1-t0)/CLOCKS_PER_SEC);
+	std::cout << "Tiempo de ejecucion: " << time << " segundos\n" ;
     return 0;
 }
